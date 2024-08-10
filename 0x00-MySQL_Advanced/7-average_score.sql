03ea55fc193b20e1d4c4a984dd67ee4bc1a43cb2
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-11 01:59:23 +0300

    test

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,12 @@
--- Write Write a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
--- Requirements:
--- Your script must create a stored procedure ComputeAverageScoreForUser
--- This stored procedure must take 1 parameter: the user_id
--- The average score must be stored in the average_score column of the users table
--- The average score must be rounded to 2 decimal places
--- The average score must be computed as the average of all the scores of the corrections of the user
-
+--  creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-    UPDATE users
-    SET average_score = ROUND((SELECT AVG(score) FROM corrections WHERE user_id = user_id), 2)
-    WHERE id = user_id;
-END$$
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
