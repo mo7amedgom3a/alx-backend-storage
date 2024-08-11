18601ba16cdac683ba62e9b875ebc282acf3dee6
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-11 03:01:51 +0300

    test

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,3 @@
--- Create an index on the name column of the names table
-CREATE INDEX idx_name_first ON  names(name);
+-- Create an index on the name column of the names table and on first character 
+DROP INDEX idx_name_first ON names;
+CREATE INDEX idx_name_first ON  names(name(1));
