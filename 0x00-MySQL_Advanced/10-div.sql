e86451a510a4d8d3e2ad56eb133032d3268c1126
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-11 03:41:28 +0300

    test

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,3 +1,5 @@
+-- Create a function named SafeDiv that takes two integers as input and returns the result of the division of the first number by the second number. If the second number is 0, the function should return 0.
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
