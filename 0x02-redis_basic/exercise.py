62fb6801f446d755031c3d996661dd3dba81fa2a
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-17 04:31:40 +0300

    test

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,17 +7,33 @@ import redis
 import uuid
 from typing import Union, Optional, Callable
 
+
 class Cache:
     """create a Cache class"""
-
 
     def __init__(self):
         """initialize a Cache instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate a random key, store the input data in Redis using the key
         and return the key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """get the data stored at the key and return it"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """get the data stored at the key and return it as a string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """get the data stored at the key and return it as an int"""
+        return self.get(key, int)
