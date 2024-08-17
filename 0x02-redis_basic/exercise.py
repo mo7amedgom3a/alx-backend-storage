b54a24980bbd7f620250eaec846b6e3a7c34dc20
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-08-17 04:58:10 +0300

    test

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -59,6 +59,16 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """display the history of calls of a particular function"""
+    r = redis.Redis()
+    method_name = method.__qualname__
+    inputs = r.lrange(method_name + ":inputs", 0, -1)
+    outputs = r.lrange(method_name + ":outputs", 0, -1)
+    print(f"{method_name} was called {r.get(method_name).decode('utf-8')} times:")
+    for i, o in zip(inputs, outputs):
+        print(f"{method_name}(*{i.decode('utf-8')}) -> {o.decode('utf-8')}")
+
 class Cache:
     """Create a Cache class"""
 
